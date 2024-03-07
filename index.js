85d87894c08777a40ab00dc4fba2a62061685c7a
littlegreen-games <txbac1998@gmail.com>
2024-03-07 06:54:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 let data = [
-    // {
-    //     id: 0, title: "Axie Tri-Force", description: 
-    //     "This casual game combines Little Green Games and SkyMavis, with an axie character inspired by the popular SkyMavis game Axie Infinity ",
-    //     image: "./crossads/images/com.SkyMavis.AxieMiniTriforce.png",
-    //     imageGooglePlay: "./imageWeb/gitbook.png",
-    //     linkGooglePlay: "https://littlegreen.gitbook.io/axie-mini-tri-force/",
-    //     textHeaderGooglePlay: "Gitbook", textMoreGooglePlay: "(available)",
-    //     imageAppStore: "./imageWeb/skymavis.png",
-    //     textHeaderAppStore: "Mavis Hub", textMoreAppStore: "(available)",
-    //     linkAppStore: "https://welcome.skymavis.com/download/",
-    // }
+    {
+        id: 0, title: "Axie Tri-Force", description: 
+        "This casual game combines Little Green Games and SkyMavis, with an axie character inspired by the popular SkyMavis game Axie Infinity ",
+        image: "./crossads/images/com.SkyMavis.AxieMiniTriforce.png",
+        imageGooglePlay: "./imageWeb/gitbook.png",
+        linkGooglePlay: "https://littlegreen.gitbook.io/axie-mini-tri-force/",
+        textHeaderGooglePlay: "Gitbook", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/skymavis.png",
+        textHeaderAppStore: "Mavis Hub", textMoreAppStore: "(available)",
+        linkAppStore: "https://welcome.skymavis.com/download/",
+    },
+    {
+        id: 1, title: "Only Way Is UP", description: 
+        "Prepare to push your limits and embark on an adrenaline-fueled adventure in Only Way Is UP - Climb Parkour! This dynamic and heart-racing game is the perfect fusion of parkour and climbing, offering an exhilarating experience that will keep you on the edge of your seat.",
+        image: "./crossads/images/com.onlywayisup.climbparkour.png",
+        imageGooglePlay: "./imageWeb/googleplay.png",
+        linkGooglePlay: "https://drive.google.com/file/d/14bXehyUJxSm1QWwOIV0xBkdY9aqVcE-k/view?usp=sharing",
+        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/itchio.png",
+        textHeaderAppStore: "Website", textMoreAppStore: "(available)",
+        linkAppStore: "https://littlegreengames.itch.io/only-way-is-up",
+    }
 ];
 
 
