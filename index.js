d932cdb3071225586e12ac1d0cc90aec556bda18
littlegreen-games <txbac1998@gmail.com>
2024-03-07 08:12:05 -0800

    update web

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 let data = [
     {
-        id: 0, title: "Axie Tri-Force", description: 
-        "This casual game combines Little Green Games and SkyMavis, with an axie character inspired by the popular SkyMavis game Axie Infinity ",
-        image: "./crossads/images/com.SkyMavis.AxieMiniTriforce.png",
-        imageGooglePlay: "./imageWeb/gitbook.png",
-        linkGooglePlay: "https://littlegreen.gitbook.io/axie-mini-tri-force/",
-        textHeaderGooglePlay: "Gitbook", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/skymavis.png",
-        textHeaderAppStore: "Mavis Hub", textMoreAppStore: "(available)",
-        linkAppStore: "https://welcome.skymavis.com/download/",
+        id: 0, title: "Jump into the Sky (Mavis)", description: 
+        "Jump, parkour, and earn Ronin tokens in the sky – it's all free!",
+        image: "./crossads/images/com.onlywayisup.climbparkour.png",
+        imageIcon: "./imageWeb/website.png",
+        linkIcon: "https://littlegreengames.itch.io/only-way-is-up",
+        textHeaderIcon: "Website", textMoreIcon: "(free to earn)",
     },
     {
-        id: 1, title: "Only Way Is UP", description: 
-        "Prepare to push your limits and embark on an adrenaline-fueled adventure in Only Way Is UP - Climb Parkour! This dynamic and heart-racing game is the perfect fusion of parkour and climbing, offering an exhilarating experience that will keep you on the edge of your seat.",
-        image: "./crossads/images/com.onlywayisup.climbparkour.png",
-        imageGooglePlay: "./imageWeb/googleplay.png",
-        linkGooglePlay: "https://drive.google.com/file/d/14bXehyUJxSm1QWwOIV0xBkdY9aqVcE-k/view?usp=sharing",
-        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/itchio.png",
-        textHeaderAppStore: "Website", textMoreAppStore: "(available)",
-        linkAppStore: "https://littlegreengames.itch.io/only-way-is-up",
+        id: 1, title: "Axie Tri-Force", description: 
+        "First, Little Green Games and SkyMavis have combined forces, and their creation is now available on Greenlight.",
+        image: "./crossads/images/com.SkyMavis.AxieMiniTriforce.png",
+        imageIcon: "./imageWeb/skymavis.png",
+        linkIcon: "https://welcome.skymavis.com/download/",
+        textHeaderIcon: "Mavis Hub", textMoreIcon: "(available)",
+    },
+    {
+        id: 2, title: "Axie Backpack", description: 
+        "Turns are your chance to make moves, shoot, use skills, and engage in combat actions, all packed with excitement.",
+        image: "./crossads/images/com.SkyMavis.AxieBackpack.png",
+        imageIcon: "./imageWeb/skymavis.png",
+        linkIcon: "https://welcome.skymavis.com/download/",
+        textHeaderIcon: "Mavis Hub", textMoreIcon: "(available)",
     }
 ];
 
@@ -28,10 +30,10 @@ let renderHtml = function (arr) {
     let html = "";
     arr.map((item, index) => {
         html += `
-        <div class="row boder mt-4" style = "background-color: #ffffff">
+        <div class="row boder mt-4" style = "background-color: #50C4ED">
                <div class="col-lg-3 " style=" padding: 25px">
                         <div class=" ">
-                            <img style="border-radius: 10px; width: 70%;"
+                            <img style="border-radius: 10px; width: 90%;"
                                 class="img-fluid "
                                 alt=""
                                 src="${item.image}">
@@ -40,36 +42,24 @@ let renderHtml = function (arr) {
                     <div class="col-lg-9">
                         <div class="row text-left mt-4">
                             <div class="text-left">
-                                <h4>${item.title}</h4>
+                                <h3>${item.title}</h3>
                             </div>
                             <div>
-                                <p style = "font-size: 12px;">${item.description}</p>
+                                <p style = "font-size: 16px;">${item.description}</p>
                             </div>
                         </div>
                         <div class="row text-left mt-2" id="listButton">
-                            <div class="col-lg-4" onclick="openLink(this)" data-link="${item.linkGooglePlay}">
+                            <div class="col-lg-4" onclick="openLink(this)" data-link="${item.linkIcon}">
                                 <div class="btn btn-info button" style="">
                                     <div style="padding: 5px;">
-                                        <img src="${item.imageGooglePlay}" alt="" style="width: 30px; height: 30px; padding: 5px">
+                                        <img src="${item.imageIcon}" alt="" style="width: 50px; height: 50px; padding: 5px">
                                     </div>
-                                    <div style="margin-left: 30px;">
-                                        <span style="font-size: 14px; padding: 0px;margin: 0px;">${item.textHeaderGooglePlay}</span>
-                                        <p style="font-size: 12px;padding: 0px; margin: -5px;">${item.textMoreGooglePlay}</p>
+                                    <div style="margin-left: 20px; margin-top: 5px;">
+                                        <span style="font-size: 18px; padding: 0px;margin: 0px;">${item.textHeaderIcon}</span>
+                                        <p style="font-size: 16px;padding: 0px; margin: 0px;">${item.textMoreIcon}</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-4" onclick="openLink(this)" data-link="${item.linkAppStore}">
-                                <div class="btn btn-info button" style="">
-                                    <div style="padding: 5px;">
-                                        <img src="${item.imageAppStore}" alt="" style="width: 30px; height: 30px; padding: 5px">
-                                    </div>
-                                    <div style="margin-left: 30px;">
-                                        <span style="font-size: 14px; padding: 0px; margin: 0px;">${item.textHeaderAppStore}</span>
-                                        <p style="font-size: 12px;padding: 0px;margin: -5px;">${item.textMoreAppStore}</p>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
                     </div>
         </div>
